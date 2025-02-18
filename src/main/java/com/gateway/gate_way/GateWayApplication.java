569dea9e163928145029f00d85a396d398efd351
Ramanjaneyulu Randhi <ramanjaneyulu.randhi@gmail.com>
2025-02-18 10:39:48 +0530

    api gateway initial commit

---
+++ src/main/java/com/gateway/gate_way/GateWayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.gate_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+}
